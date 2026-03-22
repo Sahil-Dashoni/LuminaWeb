c8c55ae8773239769c5082a49c04d8cb138190c5
Sahil Dashoni <sahildashoni453@gmail.com>
2026-03-22 20:07:07 +0530

    Update cookie settings for secure authentication

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -15,8 +15,8 @@ export const googleAuth = async (req, res) => {
 
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,
-            sameSite: "strict",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 
         });
@@ -30,8 +30,8 @@ export const logout =async (req, res) => {
     try {
         res.clearCookie("token",{
             httpOnly: true,
-            secure: false,
-            sameSite: "strict",
+            secure: true,
+            sameSite: "none",
         });
 
         return res.status(200).json({ message: "User logged out successfully" });
