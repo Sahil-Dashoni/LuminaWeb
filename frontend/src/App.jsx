10802ff52a781b44a1139d0d539aaa62e927a738
Sahil Dashoni <sahildashoni453@gmail.com>
2026-03-22 20:05:46 +0530

    Update server URL to production endpoint

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ import LiveSite from './pages/LiveSite';
 import Pricing from './pages/Pricing';
 
 
-export const serverUrl = "http://localhost:8000";
+export const serverUrl = "https://luminaweb-fngb.onrender.com";
 
 function App() {
   useGetCurrentUser()
